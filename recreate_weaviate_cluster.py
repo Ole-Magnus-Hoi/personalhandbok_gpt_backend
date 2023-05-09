92908668d3a4bcf2939ac876679fba8b9b0005a3
Ole-Magnus Høiback <ole.m.hoiback@gmail.com>
2023-05-09 12:47:46 +0200

    med re

--- recreate_weaviate_cluster.py
+++ recreate_weaviate_cluster.py
@@ -3,11 +3,12 @@ import openai
 import os
 import json
 import PyPDF2
+import re
 from math import ceil
 
 """
 Denne koden leser pdf-er fra mappen pdf_data. Der ligger personalhåndboken.
-For øyeblikket brukes kun clusters fra weaviate med varighet 14 dager. Må derfor lage nytt clusters etter det.
+For øyeblikket brukes kun clusters fra weaviate med varighet 14 dager. Må derfor lage nytt clusters etter det. Man kan bruke denne koden på ferskt cluster
 Leser over pdf-ene og lager litt mindre biter av det. Bruker embedding til open ai for å finne en plass til det i databasen.
 Er ikke ment å kjøre i "prod", men fint å ha med resten av repoet.
 """
@@ -52,8 +53,8 @@ for filename in os.listdir(pdf_dir):
                 name_and_page = filename+str(p)
                 text = pdf_reader.pages[p].extract_text()
                 text = clean(text)
-                if len(text)>750:
-                    group_size = 500
+                if len(text)>250:
+                    group_size = 250
                     overlap_size = 50
                     new_paras = [text[i:i+group_size] for i in range(0, len(text), group_size-overlap_size)]
                     for n in range(len(new_paras)):
@@ -67,12 +68,49 @@ with open('output.json', 'w') as f:
 openai.api_key = os.environ["OPENAI_KEY"]
 
 client = weaviate.Client(
-    url=weaviate_url,
-    auth_client_secret=weaviate.auth.AuthApiKey(api_key=weaviate_key),
+    url=weaviate_test_url,
+    auth_client_secret=weaviate.auth.AuthApiKey(api_key=weaviate_test_key),
     additional_headers={
         "X-OpenAI-Api-Key": openai_key
     }
 )
+
+#Nytt schema
+schema = {
+  "classes": [
+    {
+      "class": "Personalbok",
+      "description": "Innhold fra personalhåndboken til Norconsult.",
+      "vectorizer": "text2vec-openai",
+      "moduleConfig": {
+        "text2vec-openai": {
+          "model": "ada",
+          "modelVersion": "002",
+          "type": "text"
+        }
+      },
+      "properties": [
+        {
+          "dataType": [
+            "text"
+          ],
+          "description": "Avsnitt fra personalhåndboken",
+          "moduleConfig": {
+            "text2vec-openai": {
+              "skip": "false",
+              "vectorizePropertyName": "false"
+            }
+          },
+          "name": "content"
+        }
+      ]
+    }
+  ]
+}
+
+#Lage nytt schema
+client.schema.create(schema)
+#client.schema.delete_all()
 
 #Her får vi inn tekst av personalhåndboka
 
